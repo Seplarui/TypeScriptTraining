a4035aaf36024c5d43537d73c573fa0c8ec57232
Sebas <seplarui@gmail.com>
2021-03-28 13:14:39 +0200

    sample-types

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,10 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = 'Kumar';
+console.log(found);
+console.log(grade);
+console.log(firstName);
+console.log(lastName);
+//template strings
+console.log("Hi " + firstName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,14 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Anup";
+let lastName: string = 'Kumar';
+
+
+console.log(found);
+console.log(grade);
+console.log(firstName);
+console.log(lastName);
+
+//template strings
+
+console.log(`Hi ${firstName}`);
